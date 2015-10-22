f1b5f600aabd7dbb7942f63568f8da02bcd01fc4
Arshad Chummun <arshad@chummun.com>
2015-10-22 15:49:55 +0400

    Open Restaurant 7.x-1.0-rc2
    See the release notes: https://www.drupal.org/node/2599006

--- profiles/restaurant/scripts/release.sh
+++ profiles/restaurant/scripts/release.sh
@@ -4,11 +4,25 @@
 source ./config.sh
 
 # New tag.
-new_tag=$1
+current_tag=7.x-$1
+current_branch=7.x-$2
+new_tag=7.x-$3
 
 # Move up on level into the profile root.
 cd ..
 TARGET=`pwd -P`
+
+# Remove current release notes
+rm $TARGET/RELEASE_NOTES.txt
+
+echo ""
+echo ""
+echo "----------------------------------"
+echo "Profile: Open Restaurant"
+echo "----------------------------------"
+cd $TARGET
+echo "Updating the version number in build-restaurant.make to $3..."
+perl -i -pe "s/^projects\[restaurant\]\[version\]\s?=\s?(.*)$/projects[restaurant][version] = ${3}/g" build-restaurant.make
 
 # Open Restaurant modules.
 for module in "${modules[@]}"
@@ -20,26 +34,37 @@ do
   echo "----------------------------------"
   cd $TARGET/modules/restaurant/$module
 
-  # Get the latest tag.
-  current_tag="$(git describe --abbrev=0 --tags)"
+  # Create the CHANGELOG.txt
+  echo "Creating CHANGELOG.txt for $module with tag $current_tag and branch $current_branch..."
+  drush rn $current_tag $current_branch --changelog >> CHANGELOG.txt
 
-  # Tag should match 7.x-1.0-rc1
-  if [[ $current_tag =~ ^[78]+\.x\-[0-9]+\.[0-9]+ ]] ; then
-    # Create release notes using current tag.
-    echo "Creating CHANGELOG.txt for $module..."
-    drush rn $current_tag 7.x-1.x > CHANGELOG.txt
-#    git push github --tags
-#    git push drupal --tags
-  else
-    # If no tag is found then we have not created a tag yet i.e first release.
-    # Create the tag and push it.
-    echo "Creating $new_tag release for $module..."
-    git tag $new_tag
+  # Replace the current branch with the new tag.
+  echo "Updating branch $current_branch in CHANGELOG.txt to $new_tag..."
+  perl -i -pe "s/${current_branch}/${new_tag}/g" CHANGELOG.txt
 
-    git push github --tags
-    git push drupal --tags
-  fi
+  # Replace date.
+  current_date=`date +%Y-%m-%d`
+  echo "Updating the date to $current_date..."
+  perl -i -pe "s/%ad/${current_date}/g" CHANGELOG.txt
+
+  # Fix empty releases.
+  empty_text="- No changes since last release."
+  perl -i -pe "s/^\-\s?\.$/${empty_text}/g" CHANGELOG.txt
+
+  # Update version in .info file.
+  echo "Updating the version number in $module.info..."
+  perl -i -pe "s/^version\s?=\s?(.*)$/version = ${new_tag}/g" $module.info
+
+  # Update release notes.
+  # Use this to create a release on drupal.org
+  cat CHANGELOG.txt >> $TARGET/RELEASE_NOTES.txt
+
+  # Update versions in drupal.org.make
+  cd $TARGET
+  echo "Updating the version number in drupal-org.make to $3..."
+  perl -i -pe "s/^projects\[${module}\]\[version\]\s?=\s?(.*)$/projects[${module}][version] = ${3}/g" drupal-org.make
 done
+
 
 # Open Restaurant themes.
 for theme in "${themes[@]}"
@@ -51,22 +76,33 @@ do
   echo "----------------------------------"
   cd $TARGET/themes/$theme
 
-  # Get the latest tag.
-  current_tag="$(git describe --abbrev=0 --tags)"
+  # Create the CHANGELOG.txt
+  echo "Creating CHANGELOG.txt for $theme with tag $current_tag and branch $current_branch..."
+  drush rn $current_tag $current_branch --changelog >> CHANGELOG.txt
 
-  # Tag should match 7.x-1.0-rc1
-  if [[ $current_tag =~ ^[78]+\.x\-[0-9]+\.[0-9]+ ]] ; then
-    # Create release notes using current tag.
-    echo "Creating CHANGELOG.txt for $theme..."
-    drush rn $current_tag 7.x-1.x > CHANGELOG.txt
-  else
-    # If no tag is found then we have not created a tag yet i.e first release.
-    # Create the tag and push it.
-    echo "Creating $new_tag release for $theme..."
-    git tag $new_tag
+  # Replace the current branch with the new tag.
+  echo "Updating branch $current_branch in CHANGELOG.txt to $new_tag..."
+  perl -i -pe "s/${current_branch}/${new_tag}/g" CHANGELOG.txt
 
-    # Push the tags.
-    git push github --tags
-    git push drupal --tags
-  fi
+  # Replace date.
+  current_date=`date +%Y-%m-%d`
+  echo "Updating the date to $current_date..."
+  perl -i -pe "s/%ad/${current_date}/g" CHANGELOG.txt
+
+  # Fix empty releases.
+  empty_text="- No changes since last release."
+  perl -i -pe "s/^\-\s?\.$/${empty_text}/g" CHANGELOG.txt
+
+  # Update version in .info file.
+  echo "Updating the version number in $theme.info..."
+  perl -i -pe "s/^version\s?=\s?(.*)$/version = ${new_tag}/g" $theme.info
+
+  # Update release notes.
+  # Use this to create a release on drupal.org
+  cat CHANGELOG.txt >> $TARGET/RELEASE_NOTES.txt
+
+  # Update versions in drupal.org.make
+  cd $TARGET
+  echo "Updating the version number in drupal-org.make to $3..."
+  perl -i -pe "s/^projects\[${theme}\]\[version\]\s?=\s?(.*)$/projects[${theme}][version] = ${3}/g" drupal-org.make
 done
